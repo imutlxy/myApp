93c866f41326139cb5e8ad79dc0ba2311095780b
imutlxy <1162981617@qq.com>
2016-09-25 23:41:24 +0800

    first commit

---
+++ src/page/SplashComponent.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity
+} from 'react-native';
+
+export default class SplashComponent extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text >welcome to ocs !I am xinyu.liang</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
